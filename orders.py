33e74c1933827dae42dbe37a432405b84e6a04e1
Jessica Wynn <Jessicashinjo@gmail.com>
2016-08-09 10:29:56 -0500

    changed pickling format

--- orders.py
+++ orders.py
@@ -3,11 +3,20 @@ import uuid
 
 class Order:
     """ Creates new orders and reads orders that were created. """
-    def __init__(self):
-        """ Holds unpickled data """
-        self.orders_deserialized = []
+    def __init__(self, customer_id, payment_id):
+        """  """
+        self.customer_UUID = customer_id
+        self.payment_UUID = payment_id
+        self.order_UUID = uuid.uuid4().int
+        self.order_data = {
+                        "order_UUID": self.order_UUID,
+                        "customer_UUID": self.customer_UUID,
+                        "payment_UUID": self.payment_UUID,
+                        "paid": False
+                        }
+        self.create_order()
 
-    def create_order(self, customer_id, payment_id):
+    def create_order(self):
         """ Writes a new order to the orders.txt CSV file.
 
         Keyword arguements:
@@ -15,23 +24,9 @@ class Order:
         payment_id -- id of the payment that the customer chose to use
         Uses the pickle module to serialize data from the CSV file.
          """
-        order_id = uuid.uuid4().int
-        try:
-            self.read_orders()
-        except EOFError:
-            pass
-        except FileNotFoundError:
-            print("I'm a potato!")
 
-        with open("orders.txt", "wb+") as pickle_file:
-            order_data = {
-                "order_UUID": order_id,
-                "customer_UUID": customer_id,
-                "payment_UUID": payment_id,
-                "paid": False
-            }
-            self.orders_deserialized.append(order_data)
-            pickle.dump(self.orders_deserialized, pickle_file)
+        with open("orders.txt", "ab+") as pickle_file:
+            pickle.dump(self.order_data, pickle_file)
 
     def read_orders(self):
         """ Reads all the orders in the orders.txt CSV file.
@@ -39,12 +34,18 @@ class Order:
         Keyword arguments: None
         Uses the pickle module to deserialize data from the csv file.
          """
+        self.orders_deserialized = []
         with open("orders.txt", "rb+") as pickle_file:
-            self.orders_deserialized = pickle.load(pickle_file)
+            while True:
+                try:
+                    self.orders_deserialized.append(pickle.load(pickle_file))
+                except FileNotFoundError:
+                    print("I'm a potato!")
+                except EOFError:
+                    break
             print(self.orders_deserialized)
             return self.orders_deserialized
 
 if __name__ == '__main__':
-    order = Order()
-    # order.create_order(1234, 4567)
-    order.read_orders()
+    Order.read_orders()
+    # Order(1234, 4567)

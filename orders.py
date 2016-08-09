4141c03b3034479ab1cde6ffde77d484432c4462
Jessica Wynn <Jessicashinjo@gmail.com>
2016-08-08 22:10:18 -0500

    docstrings for orders module

--- orders.py
+++ orders.py
@@ -2,13 +2,19 @@ import pickle
 import uuid
 
 class Order:
-    """ """
+    """ Creates new orders and reads orders that were created. """
     def __init__(self):
-        """ Hold unpickled data """
+        """ Holds unpickled data """
         self.orders_deserialized = []
 
     def create_order(self, customer_id, payment_id):
-        """ """
+        """ Writes a new order to the orders.txt CSV file.
+
+        Keyword arguements:
+        customer_id -- the id of the customer creating the order_id
+        payment_id -- id of the payment that the customer chose to use
+        Uses the pickle module to serialize data from the CSV file.
+         """
         order_id = uuid.uuid4().int
         try:
             self.read_orders()
@@ -28,7 +34,11 @@ class Order:
             pickle.dump(self.orders_deserialized, pickle_file)
 
     def read_orders(self):
-        """ """
+        """ Reads all the orders in the orders.txt CSV file.
+
+        Keyword arguments: None
+        Uses the pickle module to deserialize data from the csv file.
+         """
         with open("orders.txt", "rb+") as pickle_file:
             self.orders_deserialized = pickle.load(pickle_file)
             print(self.orders_deserialized)

eefe2654bfddc99d0e2c107e4a343ac425220c5b
Mark Easterling <marke.easterling@gmail.com>
2016-08-09 21:18:31 -0500

    added docstrings to order_line _item.py

--- order_line_item.py
+++ order_line_item.py
@@ -2,6 +2,15 @@ import pickle
 import uuid
 
 class OrderLineItem:
+    '''
+    Creates a order line item object, used to associate multiple product
+    ID numbers with a single order ID number, creating the relationship between
+    adding products to an order
+
+    keyword arguments:
+    order_ID: derived from active order
+    product_ID: derived from products that are added to the active order
+    '''
 
 
     def __init__(self, order_ID, product_ID):
@@ -14,11 +23,20 @@ class OrderLineItem:
         self.serialize()
 
     def serialize(self):
+        '''
+        using pickle, serializes the order_line_item_object, writing it to a
+        text file.
+        '''
         with open('order_line_items.txt', 'ab+') as file:
             pickle.dump(self.order_line_item_object, file)
 
     @staticmethod
     def read_order_line_items():
+        '''
+        using pickle, deserializes order_line_item_objects from txt file
+        and adds each item to a list, which is then returned, allowing access
+        to order line item data in other modules
+        '''
         order_line_items_list = []
         with open('order_line_items.txt', 'rb') as file:
             while True:
@@ -35,6 +53,6 @@ class OrderLineItem:
 
 
 
-if __name__ == '__main__':
-    OrderLineItem.read_order_line_items()
+# if __name__ == '__main__':
+#     OrderLineItem.read_order_line_items()
 

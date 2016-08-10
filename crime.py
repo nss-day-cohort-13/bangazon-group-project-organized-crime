3ccf50ed6e7296746acad37c20f1f4c2012aa7bd
Jessica Wynn <Jessicashinjo@gmail.com>
2016-08-09 20:50:00 -0500

    functions to set current order and view all of the users past orders

--- crime.py
+++ crime.py
@@ -6,20 +6,32 @@ class Crime:
     def __init__(self):
         self.active_user = ""
         self.payment_choice = ""
+        self.payment_id = ""
+        # self.payment_id = []
         self.product_list = ""
+        self.current_order_number = ""
 
     def get_payment_options(self):
         active_user_payments = []
         all_payments = Payment.read_payments()
         for payment in all_payments:
             if self.active_user == payment["customer id"]:
-                active_user_payments.append(payment[payment])
+                active_user_payments.append(payment)
+            else:
+                # prompt to create payment option
 
-    # def set_payment_type(self):
-    #     pass
+    def set_order_number(self):
+        if self.current_order = "" && self.active_user != "":
+            self.current_order_number = Customer(self.active_user, self.payment_id)
+        else:
+            print(self.current_order)
 
-    def order(self):
-        pass
+    def all_user_orders(self):
+        active_user_past_orders = []
+        all_orders = Orders.read_orders()
+        for order in all_orders:
+            if self.active_user == order["customer_UUID"]:
+                active_user_past_orders.append(order])
 
     def line_items(self):
         pass

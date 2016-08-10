db54aeddebb761613103c7bf46e38808a9831ebb
Damon Romano <rdromano@mac.com>
2016-08-10 10:02:07 -0500

    Fixed typo in products.py init docstring

--- products.py
+++ products.py
@@ -11,9 +11,6 @@ class Product:
         read_products()
     """
     def __init__(self, name_of_product, unit_cost_of_product):
-    """
-
-    """
         self.unpickled_product_data = []
         self.product_UUID = uuid.uuid4().int
         self.name_of_product = name_of_product

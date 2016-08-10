8ec19c8a0e77937b0867d8171ff22fc428fff51a
Damon Romano <rdromano@mac.com>
2016-08-10 09:57:09 -0500

    Add docstring to product.py.

--- products.py
+++ products.py
@@ -2,8 +2,18 @@ import uuid
 import pickle
 
 class Product:
+    """
+    The Product class stores the UUID, name, and unit price of products.
 
+    Methods on this class:
+        __init__()
+        serailize_product()
+        read_products()
+    """
     def __init__(self, name_of_product, unit_cost_of_product):
+    """
+
+    """
         self.unpickled_product_data = []
         self.product_UUID = uuid.uuid4().int
         self.name_of_product = name_of_product
@@ -18,12 +28,20 @@ class Product:
         self.serialize_product()
 
     def serialize_product(self):
+    """
+    This method uses pickle.dump to write and serialize product data and
+    store it in the products.txt file.
+    """
         with open ('products.txt', 'ab+') as pickle_products_file:
             pickle.dump(self.product_object, pickle_products_file)
 
 
     @staticmethod
     def read_products():
+    """
+    This method uses pickle.load to deserialize and read product data from
+    the products.txt file.
+    """
         product_list = []
         with open ('products.txt', 'rb') as pickle_products_file:
             while True:

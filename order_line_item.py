e0487221167a6bf3c688349b657ef4b19e3f1ef3
Mark Easterling <marke.easterling@gmail.com>
2016-08-09 20:34:05 -0500

    added serialize and deserialize functions

--- order_line_item.py
+++ order_line_item.py
@@ -8,11 +8,33 @@ class OrderLineItem:
         self.order_UUID = order_ID
         self.product_UUID = product_ID
         self.order_line_item_id = uuid.uuid4().int
+        self.order_line_item_object = {"order_line_item_UUID": self.order_line_item_id,
+                                       "order_UUID": self.order_UUID,
+                                       "product_UUID": self.product_UUID}
+        self.serialize()
+
+    def serialize(self):
+        with open('order_line_items.txt', 'ab+') as file:
+            pickle.dump(self.order_line_item_object, file)
+
+    @staticmethod
+    def read_order_line_items():
+        order_line_items_list = []
+        with open('order_line_items.txt', 'rb') as file:
+            while True:
+                try:
+                    order_line_items_list.append(pickle.load(file))
+                except EOFError:
+                    break
+
+            print(order_line_items_list)
+            return order_line_items_list
 
 
 
 
 
-# if __name__ == '__main__':
 
+if __name__ == '__main__':
+    OrderLineItem.read_order_line_items()
 
